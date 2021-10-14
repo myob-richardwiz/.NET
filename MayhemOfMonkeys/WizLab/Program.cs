bd7e6547fe9bcf6a6e6179a937eb21a4f76095a6
MYOB\Richard.Wisbey <richard.wisbey@myob.com>
2021-10-14 14:56:58 +1000

    Initial Commit

---
+++ MayhemOfMonkeys/WizLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WizLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
